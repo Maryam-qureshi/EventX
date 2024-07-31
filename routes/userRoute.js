6c6a156113d64f1730c2acb9fe8ad45e42a55d62
Maryam-qureshi <maryamqureshies7@gmail.com>
2024-08-01 01:26:43 +0500

    Added userRoutes

---
+++ routes/userRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const userController = require('../controllers/UserController');
+
+const router = express.Router();
+
+
+router.post('/signup', userController.signup);
+router.post('/login', userController.login);
+router.get('/me',  userController.getUser);
+
+module.exports = router;
