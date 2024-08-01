182f4beb24119660c8e65745e97ff878ada49e15
Maryam-qureshi <maryamqureshies7@gmail.com>
2024-08-01 15:39:07 +0500

    Added user controller functions

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,51 +2,79 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-exports.signup = async (req, res) => {
+signup = async (req, res) => {
+  console.log(req.body);
   const { name, email, password, isPlanner } = req.body;
   try {
     let user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({ msg: 'User already exists' });
     }
-    user = new User({ name, email, password, isPlanner });
+    const isPlannerBoolean = (isPlanner === 'true' || isPlanner === true);
+
+    
+    user = new User({ name, email, password, isPlanner:isPlannerBoolean });
     await user.save();
-    const payload = { user: { id: user.id } };
-    jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 360000 }, (err, token) => {
-      if (err) throw err;
-      res.json({ token });
-    });
+    res.status(201).json({message: "User Successfully added", id: user.id});
+    //const payload = { user: { id: user.id } };
+    //jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 360000 }, (err, token) => {
+    //  if (err) throw err;
+    //  res.json({ token });
+    //});
   } catch (err) {
+    console.log(err);
     res.status(500).send('Server Error');
   }
 };
 
-exports.login = async (req, res) => {
+login = async (req, res) => {
   const { email, password } = req.body;
   try {
     let user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ msg: 'Invalid Credentials' });
     }
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = (password===user.password)
     if (!isMatch) {
       return res.status(400).json({ msg: 'Invalid Credentials' });
     }
-    const payload = { user: { id: user.id } };
-    jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 360000 }, (err, token) => {
-      if (err) throw err;
-      res.json({ token });
-    });
+    //const payload = { user: { id: user.id } };
+    //jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 360000 }, (err, token) => {
+    //  if (err) throw err;
+    //  res.json({ token });
+    //});
   } catch (err) {
+    console.log(err);
     res.status(500).send('Server Error');
   }
 };
 
-exports.getUser = async (req, res) => {
+getUser = async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select('-password');
-    res.json(user);
+    const user = await User.findById(req.params.id);
+    if(!user){
+        return res.status(400).json({message:"User not found"});
+    }
+    return res.json(user);
   } catch (err) {
+    console.log('Error');
     res.status(500).send('Server Error');
   }
 };
+
+deleteUser = async (req,res) => {
+    try {
+       await User.findByIdAndDelete(req.params.id);
+       res.status(200).json({message: "The user deleted successfully"});
+    }
+    catch(err) {
+        res.status(500).send('Server Error');
+    }
+}
+
+module.exports ={
+    signup,
+    login,
+    getUser,
+    deleteUser
+}
