4093b030c81ed88f5b13e0db4aa2ae965d4d0c17
Maryam-qureshi <maryamqureshies7@gmail.com>
2024-08-04 12:39:33 +0500

    Updated Event routes

---
+++ routes/eventRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const eventController = require("../controllers/eventController");
+const { restrictToLoggedinUsersOnly } = require("../middleware/auth");
+const router = express.Router();
+
+router.post(
+  "/new-event",
+  restrictToLoggedinUsersOnly,
+  eventController.createEvent
+);
+router.get("/", restrictToLoggedinUsersOnly, eventController.getEvents);
+router.delete("/:id", restrictToLoggedinUsersOnly, eventController.deleteEvent);
+
+module.exports = router;
