26b15d73d2d9d66febd5fe87653587b74feae2f7
Maryam-qureshi <maryamqureshies7@gmail.com>
2024-08-04 12:38:03 +0500

    Added authentication of user/event

---
+++ controllers/eventController.js
@@ -0,0 +1,57 @@
+const Event = require("../models/event");
+
+const createEvent = async (req, res) => {
+  console.log(req.body);
+  const { eventType, date, location, budget, preferences } = req.body;
+  try {
+    const event = new Event({
+      user: req.user._id,
+      eventType,
+      date,
+      location,
+      budget,
+      preferences,
+    });
+    await event.save();
+    console.log("Added");
+    res.redirect("/");
+  } catch (err) {
+    console.error("Error creating event:", err);
+    res.status(500).send("Server Error");
+  }
+};
+
+const getEvents = async (req, res) => {
+  try {
+    const events = await Event.find({ user: req.user._id });
+    if (!events) {
+      return res.json({ msg: "No event" });
+    }
+    res.render("events", { events });
+  } catch (err) {
+    console.error("Error fetching events:", err);
+    res.status(500).send("Server Error");
+  }
+};
+
+const deleteEvent = async (req, res) => {
+  const eventId = req.params.id;
+  console.log(`Attempting to delete event with ID: ${eventId}`); // Add this line to debug
+  try {
+    const result = await Event.findByIdAndDelete(eventId); // Delete event from your data source
+    if (result) {
+      res.status(204).send(); // Send a 204 No Content response
+    } else {
+      res.status(404).send("Event not found"); // Send 404 if the event was not found
+    }
+  } catch (error) {
+    console.error("Error deleting event:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+module.exports = {
+  createEvent,
+  getEvents,
+  deleteEvent,
+};
