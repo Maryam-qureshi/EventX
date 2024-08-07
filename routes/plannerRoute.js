63753462b38c8b39e61b3ff0d1e3f9fb8805089e
Maryam-qureshi <maryamqureshies7@gmail.com>
2024-08-07 22:48:19 +0500

    Modified

--- routes/plannerRoute.js
+++ routes/plannerRoute.js
@@ -1,18 +1,25 @@
 const express = require("express");
-const {
-  getPlanners,
-  getPlannerProfile,
-  createPlannerProfile,
-  updatePlannerProfile,
-  addPortfolioItem,
-} = require("../controllers/plannerController");
+const planner = require("../controllers/plannerController");
 const { restrictToLoggedinUsersOnly } = require("../middleware/auth");
 const router = express.Router();
 
-router.get("/", restrictToLoggedinUsersOnly, getPlanners); // Route to browse planners
-router.get("/:id", restrictToLoggedinUsersOnly, getPlannerProfile); // Route to view planner profile
-router.post("/profile", restrictToLoggedinUsersOnly, createPlannerProfile); // Route to create planner profile
-router.put("/profile/:id", restrictToLoggedinUsersOnly, updatePlannerProfile); // Route to update planner profile
-router.post("/portfolio", restrictToLoggedinUsersOnly, addPortfolioItem); // Route to add portfolio item
+router.get("/", restrictToLoggedinUsersOnly, planner.getPlanners); // Route to browse planners
+router.get("/:id", restrictToLoggedinUsersOnly, planner.getPlannerProfile); // Route to view planner profile
+router.post(
+  "/profile",
+  restrictToLoggedinUsersOnly,
+  planner.createPlannerProfile
+); // Route to create planner profile
+router.put(
+  "/profile/:id",
+  restrictToLoggedinUsersOnly,
+  planner.updatePlannerProfile
+); // Route to update planner profile
+router.post(
+  "/portfolio",
+  restrictToLoggedinUsersOnly,
+  planner.addPortfolioItem
+); // Route to add portfolio item
+router.delete("/:id", restrictToLoggedinUsersOnly, planner.deletePlanner); //Route to delete planner
 
 module.exports = router;
