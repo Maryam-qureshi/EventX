329affd07d8242496afda2adfa8e6158981cd553
Maryam-qureshi <maryamqureshies7@gmail.com>
2024-08-20 23:17:29 +0500

    Added socket.io

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
-const { connectMongoDb } = require("./connection/config");
+const http = require("http");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const { restrictToLoggedinUsersOnly } = require("./middleware/auth");
+const socketIo = require("socket.io");
+const { connectMongoDb } = require("./connection/config");
 const userRoutes = require("./routes/userRoute");
 const eventRoutes = require("./routes/eventRoute");
 const staticRoute = require("./routes/staticRouter");
 const plannerRoutes = require("./routes/plannerRoute");
 const taskRoutes = require("./routes/taskRoute");
+const chatRoutes = require("./routes/chatRoute");
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 const PORT = 8000;
 
 // Connect Database
@@ -29,7 +33,11 @@ app.use("/api/event", eventRoutes);
 app.use("/", staticRoute);
 app.use("/api/planners", plannerRoutes);
 app.use("/api/tasks", restrictToLoggedinUsersOnly, taskRoutes);
+app.use("/api/chat", chatRoutes); // Chat route
 
-app.listen(PORT, () => {
+// Initialize WebSocket
+require("./socket")(io);
+
+server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
